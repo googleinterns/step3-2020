1f2cb2417187b8580b044bde099b1c928fffa470
Tony Pan <tonypan2000@gmail.com>
2020-06-23 17:24:56 +0000

    edit data

--- similarity/collaborative_filter.py
+++ similarity/collaborative_filter.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 
 # Reads csv as input and returns a Pandas DataFrame
@@ -11,11 +12,17 @@ def calc_pearson(df):
   corr = df.corr(method='pearson')
   return corr
 
+def sort_index(corr):
+  sorted = np.argsort(corr, axis=1)
+  return sorted
+
 def main():
   df = read_data('theoretical_data.csv')
   # calculate pearson coefficient
   corr = calc_pearson(df)
   # find k most similar users
+  sorted = sort_index(corr)
+  print(sorted)
   # (fill in NANs with most similar orgs)
 
   # or matrix decomposition
