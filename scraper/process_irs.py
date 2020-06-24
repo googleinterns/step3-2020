2b66238c5e1fe36687f40d30117a89bd3f32c4dc
Tony Pan <tonypan2000@gmail.com>
2020-06-24 20:17:00 +0000

    get url by searching google

--- scraper/process_irs.py
+++ scraper/process_irs.py
@@ -2,13 +2,40 @@ import csv
 from googlesearch import search
 
 def read_csv(filename):
-  with open(filename, 'r') as f:
-    reader = list(csv.reader(f))
-  return reader
+  file = open(filename, 'r')
+  reader = csv.reader(file)
+  # skip the header
+  next(reader, None)
+  data = list(reader)
+  file.close()
+  return list(data)
+
+def search_google(data, start, length):
+  names = data[start: start + length]
+  url = []
+  for org in names:
+    link = [query for query in search(org[0],
+        tld='com',  # The top level domain
+        lang='en',  # The language
+        num=1,  # Number of results per page
+        start=0,  # First result to retrieve
+        pause=2.0,  # Lapse between HTTP requests
+        )]
+    print(link[0])
+    url.append(link[0])
+  return url
+
+def save_results(results, start, length):
+  with open('links_irs990_' + str(start) + '_' + str(start + length) + '.csv', 'w') as f:
+    writer = csv.writer(f, quoting=csv.QUOTE_ALL)
+    writer.writerow(results)
+
 
 if __name__ == '__main__':
-  data = read_csv('irs990_names.csv')
-  print(len(data))
+  # TODO: set the start and num_entries each run
   start = 0
-  num_entries = 10
-  # search(start, num_entries)
+  num_entries = 1
+  data = read_csv('irs990_names.csv')
+  results = search_google(data, start, num_entries)
+  save_results(results, start, num_entries)
+  
