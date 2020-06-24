347e8bd7e1bed269cf0b4db1f290a9c42908698a
hajiler <elijahrb@google.com>
2020-06-24 00:02:25 +0000

    Added functionallity to search bar. Allows finding organization by exact title, makes post request to prediction servlet.

--- website/src/main/java/com/google/step/servlets/DataServlet.java
+++ website/src/main/java/com/google/step/servlets/DataServlet.java
@@ -23,8 +23,12 @@ import com.google.appengine.api.datastore.KeyFactory;
 import com.google.appengine.api.datastore.PreparedQuery;
 import com.google.appengine.api.datastore.Query;
 import com.google.step.data.OrganizationInfo;
-import java.io.IOException;
+import com.google.gson.Gson;
+import com.opencsv.*;
+import java.io.*;
 import javax.servlet.annotation.WebServlet;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -35,9 +39,29 @@ public class DataServlet extends HttpServlet {
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    response.setContentType("text/html;");
-    response.getWriter().println("<h1>Hello world!</h1>");
-  }
+    String filename = "../../../similarity/sample_data.csv";
+    int index = searchCsvFor(request.getParameter("organization"), filename);
+    if (index != -1) {
+      URL url = new URL("https://8080-5b0203f2-347a-4641-b84a-bbaedc8c3aec.us-central1.cloudshell.dev/predict");
+      HttpURLConnection con = (HttpURLConnection) url.openConnection();
+      con.setRequestMethod("GET");
+      con.setRequestProperty("org_index", Integer.toString(index));
+      BufferedReader in = new BufferedReader(
+      new InputStreamReader(con.getInputStream()));
+      String inputLine;
+      StringBuffer content = new StringBuffer();
+      while ((inputLine = in.readLine()) != null) {
+          content.append(inputLine);
+      }
+      in.close();
+      response.setContentType("application/json; charset=UTF-8");
+      response.setCharacterEncoding("UTF-8");
+      Gson gson = new Gson();
+      response.getWriter().println(content);
+    } else {
+    response.sendRedirect("/upload.html");
+}  }
+
 
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
@@ -51,7 +75,27 @@ public class DataServlet extends HttpServlet {
       datastore.put(submission.getEntity());
       response.sendRedirect("/index.html");
     } else {
-      response.sendRedirect("/");
+    response.sendRedirect("/");
     }
+  }
+
+  private static int searchCsvFor(String orgName,String filename) {
+    try {
+      FileReader filereader = new FileReader(filename); 
+      CSVReader csvReader = new CSVReaderBuilder(filereader).withSkipLines(1).build();
+      String[] nextRecord = new String[2]; 
+      int index = 0;
+      while ((nextRecord = csvReader.readNext()) != null) {
+        System.out.println(orgName + nextRecord[0]);
+        if (nextRecord[0].equals(orgName)) {
+          return index;
+        } else {
+          index++;
+        }
+      } 
+    } catch(Exception ex) {
+      System.err.println(ex);
+    }
+    return -1;
   }
 }
