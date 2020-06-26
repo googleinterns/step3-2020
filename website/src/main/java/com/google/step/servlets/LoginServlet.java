0b239631ae72d47f3d752bc8ff5d83537b2ce10f
Iris Beckham <irisdancer3@gmail.com>
2020-06-26 14:39:26 +0000

    redirect button to go straight to google sign-in

--- website/src/main/java/com/google/step/servlets/LoginServlet.java
+++ website/src/main/java/com/google/step/servlets/LoginServlet.java
@@ -18,11 +18,16 @@ public class LoginServlet extends HttpServlet {
     UserService userService = UserServiceFactory.getUserService();
     if (userService.isUserLoggedIn()) {
       String logoutUrl = userService.createLogoutURL("/index.html");
-      response.getWriter().println("<p>Logout <a href=\"" + logoutUrl + "\">here</a>.</p>");
+      // response.getWriter().println(logoutUrl);
+      response.sendRedirect(logoutUrl);
     } else {
       String loginUrl = userService.createLoginURL("/index.html");
-      response.getWriter().println("<p>Login <a href=\"" + loginUrl + "\">here</a>.</p>");
+      // response.getWriter().println(loginUrl);
+      response.sendRedirect(loginUrl);
+
     }
   }
+
+    
 
 }
