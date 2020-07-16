9b6afa8cf350f6eb33aaf2c00729e6d0f7151b78
Tony Pan <tonypan2000@gmail.com>
2020-07-16 22:28:11 +0000

    sqrt of SVD

--- similarity/rec_sys/collab_filter_svd.py
+++ similarity/rec_sys/collab_filter_svd.py
@@ -8,18 +8,31 @@ def read_data(filename):
   # fill NANs with 0s
   return data.fillna(0)
 
-def svd(df):
+def svd(df, k):
   data = df.drop(columns=['Name'])
   u, s, v = np.linalg.svd(data, full_matrices=False)
-  prediction = np.dot(u * s, v)
-  return np.rint(prediction)
+  s_mat = np.diag(s)
+  # prediction = np.dot(u, np.dot(s_mat, v))
+  
+  # take k most significant features
+  u = u[:, 0:k]
+  s_mat = s_mat[0:k, 0:k]
+  v = v[0:k, :]
+  s_root = np.sqrt(s_mat)
+  # u root_s * root_sv
+  prediction = (u.dot(s_root)).dot(s_root.dot(v))
+
+  return prediction
 
 def main():
   df = read_data('theoretical_data.csv')
   print(df)
   # matrix decomposition with SVD
-  prediction = svd(df)
-  print(prediction)
+  prediction = svd(df, 2)
+  int_result = np.rint(prediction)
+  print(int_result)
+  # processed = edit_data(df, int_result)
+  # print(processed)
 
 
 if __name__ == '__main__':
