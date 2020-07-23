cb6e29bd8c5038f7dcbc64c0a38cf70ae4a9e229
Iris Beckham <irisdancer3@gmail.com>
2020-07-23 15:09:13 +0000

    accordion feature for technology stack

--- website/src/main/webapp/website_script.js
+++ website/src/main/webapp/website_script.js
@@ -384,3 +384,20 @@ function openSearch() {
 function closeSearch() {
   document.getElementById("myOverlay").style.display = "none";
 }
+
+function accordion(){
+  var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
+}
