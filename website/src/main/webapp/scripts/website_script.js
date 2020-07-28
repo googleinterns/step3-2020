83e39ccfadd7a28279ba3ce22be233a45ff6c31d
Tony Pan <tonypan2000@gmail.com>
2020-07-28 19:43:25 +0000

    make account log a dropdown

--- website/src/main/webapp/scripts/website_script.js
+++ website/src/main/webapp/scripts/website_script.js
@@ -466,7 +466,7 @@ function getLoginStatus() {
     if (link.includes('logout')) {
       // is logged in 
       const statusElement = document.getElementById('login-status');
-      statusElement.innerText = 'Hello!  ';
+      statusElement.innerText = 'Hello!';
       const logoutElement = document.getElementById('login-link');
       if (!link.includes('http')) {
         logoutElement.href = link;
@@ -520,7 +520,6 @@ function accordion(){
 }
 
 function openHamburger() {
-  console.log("opening hamburger");
   document.getElementById("drawer").style.display = "block";
   document.getElementById("drawer").style.zIndex="11";
 
@@ -532,11 +531,30 @@ function openHamburger() {
 }
 
 function closeHamburger() {  
-  console.log("closing hamburger");
   document.getElementById("drawer").style.display = "none";
   document.removeEventListener('click', event => {
     if (event.target.id !== 'hamburger') {
       closeHamburger();
+    }
+  });
+}
+
+function openLoginStatus() {
+  document.getElementById("login-drawer").style.display = "block";
+  document.getElementById("login-drawer").style.zIndex="11";
+
+  document.addEventListener('click', event => {
+    if (event.target.id !== 'loginIcon') {
+      closeLoginStatus();
+    }
+  });
+}
+
+function closeLoginStatus() {  
+  document.getElementById("login-drawer").style.display = "none";
+  document.removeEventListener('click', event => {
+    if (event.target.id !== 'loginIcon') {
+      closeLoginStatus();
     }
   });
 }
@@ -595,7 +613,7 @@ function getOrgNameAndId(org) {
 
 
 function setUpNavbar(){
-  document.getElementById("top-nav").innerHTML="    <header class='mdc-top-app-bar' id='app-bar' style=\"left:0%\"><div class='mdc-top-app-bar__row'><section class='mdc-top-app-bar__section mdc-top-app-bar__section--align-start'><button id='hamburger' onclick='openHamburger();' class='material-icons mdc-top-app-bar__navigation-icon mdc-icon-button' aria-label='Open navigation menu'>menu</button><a href='/index.html'><button class='material-icons mdc-top-app-bar__action-item mdc-icon-button' aria-label='Home'>home</button></a> <span class='mdc-top-app-bar__title'>Nonprofit Finder</span></section><section class='mdc-top-app-bar__section mdc-top-app-bar__section--align-end' role='toolbar'><button class='material-icons mdc-top-app-bar__action-item mdc-icon-button' aria-label='Search' onclick='openSearch()'>search</button> <p id='login-status'></p><a id='login-link'></a> <a id='loginIcon'><button class='material-icons mdc-top-app-bar__action-item mdc-icon-button' aria-label='Options'>account_circle</button></a></section></div></header>";
+  document.getElementById("top-nav").innerHTML="    <header class='mdc-top-app-bar' id='app-bar' style=\"left:0%\"><div class='mdc-top-app-bar__row'><section class='mdc-top-app-bar__section mdc-top-app-bar__section--align-start'><button id='hamburger' onclick='openHamburger();' class='material-icons mdc-top-app-bar__navigation-icon mdc-icon-button' aria-label='Open navigation menu'>menu</button><a href='/index.html'><button class='material-icons mdc-top-app-bar__action-item mdc-icon-button' aria-label='Home'>home</button></a> <span class='mdc-top-app-bar__title'>Nonprofit Finder</span></section><section class='mdc-top-app-bar__section mdc-top-app-bar__section--align-end' role='toolbar'><button class='material-icons mdc-top-app-bar__action-item mdc-icon-button' aria-label='Search' onclick='openSearch()'>search</button> <section class='mdc-top-app-bar__section mdc-top-app-bar__section--align-end' role='toolbar'><button id='loginIcon' onclick='openLoginStatus();' class='material-icons mdc-top-app-bar__action-item mdc-icon-button' aria-label='Options'>account_circle</button></section></div></header>";
 
   // document.getElementById("myOverlay").innerHTML="<span class='closebtn material-icons' onclick='closeSearch()' title='Close Overlay'><span class='material-icons'>clear</span></span><div id=’results-search’><input type=’text’ id=’keyword’ placeholder=’Search by keyword’><span class=’material-icons’ onclick=’search();’>search</span> </div>";
   
